afaac4d1036c79c30dc03036506cda20222e5e32
Thomas Stoeger <thomas.stoeger@imls.uzh.ch>
2015-04-08 14:02:43 +0200

    removed duplication of grabColumnIndex.m and fixed dangerous behaviour in grabColumnIndex.m that could lead to bugs in GUI

--- Shared/SingleCellClassification/grabColumnIndex.m
+++ Shared/SingleCellClassification/grabColumnIndex.m
@@ -1,18 +1,29 @@
 function objectIndex = grabColumnIndex(matImage, objectName)
 %GRABCOLUMNINDEX Get matrix column index from 'features' cell array
 
-matImageObjectCount = cat(1, matImage.ObjectCount{:});
 cellObjectCountFeatures = matImage.ObjectCountFeatures;
 
-if size(unique(matImageObjectCount','rows'),1)==1
-    % this means that all object count columns are equal, so it doesn't
-    % matter which one we take
-    objectIndex = 1;
-    return
-end
+% [TS150408: removed the following block since it appears quite dangerous,
+% e.g.: if no objects (of those specified in  "objectName") are present or
+% if index is reused (and by chance on first site e.g.: "nuclei" and
+% "spots" are same ] 
+%
+% matImageObjectCount = cat(1, matImage.ObjectCount{:});
+% if size(unique(matImageObjectCount','rows'),1)==1
+%     % this means that all object count columns are equal, so it doesn't
+%     % matter which one we take  
+%     objectIndex = 1;
+%     return
+% end
 
 %  otherwise, look for colum index containing object names ("Nuclei"), take that
 %  column
 objectIndex = find(cellfun(@(name) strcmp(name, objectName), cellObjectCountFeatures), 1, 'first');
+
+% [TS150408: note that one of our repositories - dep/master - has
+% implementation of upper line, which is slightly different and less 
+% stringent, namely:
+% objectIndex = find(~cellfun(@isempty,strfind(cellObjectCountFeatures, objectName)), 1 ,'first'); 
+% [TS150408 -end]
 
 end
