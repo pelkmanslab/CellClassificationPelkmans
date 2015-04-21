b7e139b3c2cf78f51135420a12859b9030c7c140
Thomas Stoeger <thomas.stoeger@imls.uzh.ch>
2015-04-21 23:07:00 +0200

    update dependencies with library

--- Shared/ATTENTIONGeneralFunctionsOfLab/+mcyc/getSiteRowColumnTimepoint.m
+++
@@ -1,8 +0,0 @@
-function SiteRowColumnTimepoint = getSiteRowColumnTimepoint(strFileName)
-
-[intSite] = check_image_position(strFileName);
-[intRow, intColumn,~, intTimepoint] = filterimagenamedata(strFileName);
-
-SiteRowColumnTimepoint = [intSite intRow intColumn intTimepoint];
-
-end
